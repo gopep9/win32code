8ba24c373093fc4085e03dd9662230a7dfd6016d
hz <3002913319>
2018-08-22 12:13:00 +0800

    添加一个能够获取复制和粘贴的钩子程序

---
+++ clipboardAndMouseHook/clipboardAndMouseHook/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 clipboardAndMouseHook.rc 使用
+//
+#define IDD_DIALOG                      101
+#define IDC_SET_HOOK_BUTTON             1002
+#define IDC_DEL_HOOK_BUTTON             1003
+#define IDC_LIST3                       1005
+#define IDC_LIST1                       1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
